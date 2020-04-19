081b120af34bf493465cee31845d21bdab53d799
Albin Metthey <a.metthey@gmail.com>
2020-04-19 19:57:56 +0200

    ajout de la signature

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 		<a href="<?php echo get_field('adresse_mail_link', 'option'); ?>" target="_blank"><?php echo get_field('adresse_mail', 'option'); ?></a>
 			<a href="<?php echo get_field('adresse_link', 'option'); ?>" target="_blank"><?php echo get_field('adresse', 'option'); ?></a>
 		</div>
+		<p class="signature">website by <a href="mailto:albin@perimetre.studio">Albin Metthey</a></p>
 		<img src="<?php echo get_field('logo', 'option'); ?>" >
 			
 	</footer>

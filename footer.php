f3f1cdb419d9b46d6cadc2aafec981e8bfed0f9b
Albin Metthey <a.metthey@gmail.com>
2020-04-23 15:44:04 +0200

    typo modification with new vfont

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<a href="<?php echo get_field('adresse_mail_link', 'option'); ?>" target="_blank"><?php echo get_field('adresse_mail', 'option'); ?></a>
 			<a href="<?php echo get_field('adresse_link', 'option'); ?>" target="_blank"><?php echo get_field('adresse', 'option'); ?></a>
 		</div>
-		<p class="signature">website by <a href="mailto:albin@perimetre.studio">Albin Metthey</a></p>
+		<p class="signature">website by <a href="mailto:albin@perimetre.studio">perimetre.studio</a></p>
 		<img src="<?php echo get_field('logo', 'option'); ?>" >
 			
 	</footer>
